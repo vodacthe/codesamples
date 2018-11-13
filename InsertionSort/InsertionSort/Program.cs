cd094279b1a04b3a756fa27687b47f21708902d6
Vo Dac Thed <lamviec.vdt@hoadatmail.com>
2018-11-13 17:27:30 +0700

    Added Insertion Sort

---
+++ InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Generate(15);
+            Print(array);
+            Console.WriteLine("PRESS ENTER TO DO INSERTION SORT");
+            Console.ReadLine();
+            InsertionSort(array);
+            Console.WriteLine("SORT COMPLETED");
+            Print(array);
+            Console.WriteLine("PRESS ENTER TO QUIT");
+            Console.ReadLine();
+        }
+
+        #region Sort
+        private static void InsertionSort(int[] input)
+        {
+            for(int i = 1; i < input.Length;i++)
+            {
+                int key = input[i];
+                int j = i - 1;
+                while(j >= 0 && key < input[j])
+                {
+                    input[j + 1] = input[j];
+                    j--;
+                }
+                input[j + 1] = key;
+            }
+        }
+        #endregion
+
+        #region Others
+        private static int[] Generate(int lenght)
+        {
+            int[] res = new int[lenght];
+            Random ran = new Random();
+            for (int i = 0; i < lenght; i++)
+            {
+                res[i] = ran.Next(-20, 20);
+            }
+            return res;
+        }
+
+        private static void Print(int[] input)
+        {
+            string message = "Array: ";
+            foreach (int i in input)
+                message += i + " ";
+            Console.WriteLine(message);
+        }
+        #endregion
+    }
+}
---
+++ InsertionSort/InsertionSort/README.txt
@@ -0,0 +1,7 @@
+﻿THIS IS SAMPLE CODING OF INSERTION SORT IN C#
+
+FOR VISUAL REFERENCES PLEASE VISIT
+https://visualgo.net/en/sorting
+
+Best case	Average		Worst case
+O(n)		O(n^2)		O(n^2)
